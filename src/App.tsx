de47015773f53f6eeb3590baf06546d385e69568
Stef Gootzen <stefgootzen@gmail.com>
2023-06-30 08:01:29 +0200

    feat: reproduce bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,58 @@
 import React from 'react';
-import { useState } from 'react';
-import { fromLonLat } from 'ol/proj';
+import {useState} from 'react';
+import {fromLonLat} from 'ol/proj';
 import 'ol/ol.css';
-import { RMap, RLayerVector, RFeature, ROverlay, ROSM, RStyle } from 'rlayers';
+import {RMap, RLayerVector, RFeature, ROverlay, ROSM} from 'rlayers';
 import Point from 'ol/geom/Point';
 import './App.css';
-import { Feature } from 'ol';
-import { Coordinate } from 'ol/coordinate';
 
-interface CustomFeature {
-	id: number,
-	longitude: number,
-	latitude: number
+const features = [0, 1, 2].map(id => ({
+  id,
+  longitude: 9.5 - 0.5 + Math.random(),
+  latitude: 51.3 - 0.5 + Math.random()
+}));
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
 }
 
 export const App = () => {
+  const [view, setView] = useState({center: fromLonLat([9.5, 51.3]), zoom: 8});
+  const [activeFeatureId, setActiveFeatureId] = useState<number | null>(null);
 
-	const [view, setView] = useState({ center: fromLonLat([9.5, 51.3]), zoom: 8 });
-	const [features, setFeatures] = useState<CustomFeature[]>([]);
-	const [features2, setFeatures2] = useState<CustomFeature[]>([]);
-
-	return (
-		<>
-		<h1>Example with ROverlay (crashes)</h1>
-		<RMap
-			initial={view}
-			view={[view, setView]}
-			className="map">
-			<ROSM />
-
-			<RLayerVector
-				zIndex={10}
-			>
-				{
-					features.map(f => <RFeature key={f.id} geometry={new Point(fromLonLat([f.longitude, f.latitude]))}>
-						{/*
-							Adding an ROverlay to a feature will cause a crash when a feature is removed
+  return (
+    <>
+      <h1>Example with ROverlay (crashes)</h1>
+      <RMap
+        initial={view}
+        view={[view, setView]}
+        className="map">
+        <ROSM/>
+        <RLayerVector
+          zIndex={10}
+        >
+          {
+            features.map(f => <RFeature key={f.id} geometry={new Point(fromLonLat([f.longitude, f.latitude]))}>
+              {/*
+							Adding an ROverlay to a feature will cause a crash when the overlay is removed
 						*/}
-						<ROverlay>
-							<div>{f.id}</div>
-						</ROverlay>
-					</RFeature>)
-				}
-			</RLayerVector>
-		</RMap>
-		<div>
-			{
-				features.map(feature => <div key={feature.id}>
-					{feature.id}
-					<button onClick={() => setFeatures(features.filter(f => f !== feature))}>Remove</button>
-				</div>)
-			}
-			<button onClick={
-				() => setFeatures((prev) => [
-					...prev,
-					{
-						id: Math.random(),
-						longitude: 9.5 - 0.5 + Math.random(),
-						latitude: 51.3 - 0.5 + Math.random()
-					}])
-			}>
-				Add
-			</button>
-		</div>
-	</>
-	);
+              {
+                (f.id === activeFeatureId) && (
+                  <ROverlay key={f.id}>
+                    <div>{f.id}</div>
+                  </ROverlay>
+                )
+              }
+            </RFeature>)
+          }
+        </RLayerVector>
+      </RMap>
+        <button onClick={
+          () => setActiveFeatureId(getRandomInt(3))
+        }>
+          Set active Feature ID
+        </button>
+    </>
+  );
 }
 
